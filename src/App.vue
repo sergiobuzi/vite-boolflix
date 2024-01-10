68cb0ff81c84ed4ba79d101ac84bb32f52acb069
SergioBuzi <sergio.buzi97@gmail.com>
2024-01-10 14:50:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+// importo axios
+import axios from 'axios';
+//importo i figli
+import AppHeader from './components/AppHeader.vue';
+//importo lo store
+import { store } from './store';
+
+
+export default {
+  components: {
+    AppHeader,
+    
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+
+<template>
+
+  <header>
+    <AppHeader />
+  </header>
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: 'AppHeader',
+
+    data() {
+            return {
+            
+            }
+        }
+}
+</script>
+
+<template>
+
+    <div id="container">
+        <h1>BOOLFLIX</h1>
+        <input type="type" placeholder="Cerca un film">
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+#container{
+    height: 70px;
+    width: 100%;
+    background-color: black;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h1{
+        margin-left: 10px;
+        color: red;
+    }
+}
+</style>
