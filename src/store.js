575ed85918dbdf39615bb99bb08c01e2215a4c31
SergioBuzi <sergio.buzi97@gmail.com>
2024-01-12 22:13:04 +0100

    config: add tv series

--- src/store.js
+++ src/store.js
@@ -3,8 +3,10 @@ import { reactive } from "vue"
 export const store = reactive({
     //array che contiene la lista dei film
     filmList: [],
-    filmURL: "https://api.themoviedb.org/3/search/movie?api_key=a18d43f8548d9db5237e978d594c8ab8&query=movie",
+    filmURL: "https://api.themoviedb.org/3/search/movie?api_key=a18d43f8548d9db5237e978d594c8ab8",
     //creo una variabile per la searchbar
     searchText: "",
-    //link per le bandiere
+    //array che contiene la lista delle serie tv
+    serieTvList: [],
+    serieTvURL: "https://api.themoviedb.org/3/search/tv?api_key=a18d43f8548d9db5237e978d594c8ab8",
 });
